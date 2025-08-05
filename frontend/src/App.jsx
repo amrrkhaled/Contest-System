dc9f129df2b4d9d73122153c1ab6ec90ede356b2
habibaelghazouly <habibahoussam7@gmail.com>
2025-08-05 16:53:45 +0300

    Add routes of About and Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Leaderboard } from './Components/Leaderboard';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Leaderboard } from "./Components/Leaderboard";
+import Home from "./Components/Home";
+import About from "./Components/About";
+//import Login from './Components/Login';
+import "./App.css";
 
 function App() {
   return (
     <div>
-    <Router>
-      <Routes>
-        <Route path="/:contestId" element={<Leaderboard />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/:contestId" element={<Leaderboard />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          {/* <Route path="/Login" element={<Login />} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
